323e019fde2ddcfa1104c1cb0b2e5436bb0446ee
Security 77 <95581252+HedyFake@users.noreply.github.com>
2022-06-01 21:17:34 +0700

    Delete itsmehedyfake.com directory

--- itsmehedyfake.com/lain/yt/load.py
+++
@@ -1,7 +0,0 @@
-import json
-import requests
-
-
-class Load:
-r = requests.get('https://youtube.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UChMIkGetnMB4qq9K5kNwkXA&key=AIzaSyBAer1fK1IHxojU0vYq1ZwzchSPmgoLBmM')
-x = json.load(r.text)
